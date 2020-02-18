86646ce191474f1ce5d346fe4f04b2c634ce0bfb
vibecode <rumashkin@gmail.com>
2020-02-18 23:00:27 +0100

    update apollo setup

--- client/generated/graphql.tsx
+++ client/generated/graphql.tsx
@@ -1,19 +1,20 @@
-import gql from 'graphql-tag'
-import * as React from 'react'
-import * as ApolloReactCommon from '@apollo/react-common'
-import * as ApolloReactComponents from '@apollo/react-components'
-import * as ApolloReactHoc from '@apollo/react-hoc'
-export type Maybe<T> = T | null
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+import gql from 'graphql-tag';
+import * as React from 'react';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as ApolloReactComponents from '@apollo/react-components';
+import * as ApolloReactHoc from '@apollo/react-hoc';
+import * as ApolloReactHooks from '@apollo/react-hooks';
+export type Maybe<T> = T | null;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-  Upload: any
-}
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+  Upload: any,
+};
 
 export enum CacheControlScope {
   Public = 'PUBLIC',
@@ -21,55 +22,61 @@ export enum CacheControlScope {
 }
 
 export type CreateTaskInput = {
-  title: Scalars['String']
-}
+  title: Scalars['String'],
+};
 
 export type Mutation = {
-  __typename?: 'Mutation'
-  createTask?: Maybe<Task>
-  updateTask?: Maybe<Task>
-  changeStatus?: Maybe<Task>
-  deleteTask?: Maybe<Task>
-}
+   __typename?: 'Mutation',
+  createTask?: Maybe<Task>,
+  updateTask?: Maybe<Task>,
+  changeStatus?: Maybe<Task>,
+  deleteTask?: Maybe<Task>,
+};
+
 
 export type MutationCreateTaskArgs = {
   input: CreateTaskInput
-}
+};
+
 
 export type MutationUpdateTaskArgs = {
   input: UpdateTaskInput
-}
+};
+
 
 export type MutationChangeStatusArgs = {
-  id: Scalars['Int']
+  id: Scalars['Int'],
   status: TaskStatus
-}
+};
+
 
 export type MutationDeleteTaskArgs = {
   id: Scalars['Int']
-}
+};
 
 export type Query = {
-  __typename?: 'Query'
-  hello?: Maybe<Scalars['String']>
-  tasks: Array<Task>
-  task?: Maybe<Task>
-}
+   __typename?: 'Query',
+  hello?: Maybe<Scalars['String']>,
+  tasks: Array<Task>,
+  task?: Maybe<Task>,
+};
+
 
 export type QueryTasksArgs = {
   status?: Maybe<TaskStatus>
-}
+};
+
 
 export type QueryTaskArgs = {
   id: Scalars['Int']
-}
+};
 
 export type Task = {
-  __typename?: 'Task'
-  id: Scalars['Int']
-  title: Scalars['String']
-  status: TaskStatus
-}
+   __typename?: 'Task',
+  id: Scalars['Int'],
+  title: Scalars['String'],
+  status: TaskStatus,
+};
 
 export enum TaskStatus {
   Active = 'active',
@@ -77,65 +84,75 @@ export enum TaskStatus {
 }
 
 export type UpdateTaskInput = {
-  id: Scalars['Int']
-  title?: Maybe<Scalars['String']>
-  status?: Maybe<TaskStatus>
-}
+  id: Scalars['Int'],
+  title?: Maybe<Scalars['String']>,
+  status?: Maybe<TaskStatus>,
+};
+
 
 export type TasksQueryVariables = {
   status?: Maybe<TaskStatus>
-}
+};
 
-export type TasksQuery = { __typename?: 'Query' } & {
-  tasks: Array<{ __typename?: 'Task' } & Pick<Task, 'id' | 'title' | 'status'>>
-}
+
+export type TasksQuery = (
+  { __typename?: 'Query' }
+  & { tasks: Array<(
+    { __typename?: 'Task' }
+    & Pick<Task, 'id' | 'title' | 'status'>
+  )> }
+);
+
 
 export const TasksDocument = gql`
-  query Tasks($status: TaskStatus) {
-    tasks(status: $status) {
-      id
-      title
-      status
-    }
+    query Tasks($status: TaskStatus) {
+  tasks(status: $status) {
+    id
+    title
+    status
   }
-`
-
-export type TasksComponentProps = Omit<
-  ApolloReactComponents.QueryComponentOptions<TasksQuery, TasksQueryVariables>,
-  'query'
->
-
-export const TasksComponent = (props: TasksComponentProps) => (
-  <ApolloReactComponents.Query<TasksQuery, TasksQueryVariables>
-    query={TasksDocument}
-    {...props}
-  />
-)
-
-export type TasksProps<TChildProps = {}> = ApolloReactHoc.DataProps<
-  TasksQuery,
-  TasksQueryVariables
-> &
-  TChildProps
-export function withTasks<TProps, TChildProps = {}>(
-  operationOptions?: ApolloReactHoc.OperationOption<
-    TProps,
-    TasksQuery,
-    TasksQueryVariables,
-    TasksProps<TChildProps>
-  >
-) {
-  return ApolloReactHoc.withQuery<
-    TProps,
-    TasksQuery,
-    TasksQueryVariables,
-    TasksProps<TChildProps>
-  >(TasksDocument, {
-    alias: 'tasks',
-    ...operationOptions
-  })
 }
-export type TasksQueryResult = ApolloReactCommon.QueryResult<
+    `;
+export type TasksComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<TasksQuery, TasksQueryVariables>, 'query'>;
+
+    export const TasksComponent = (props: TasksComponentProps) => (
+      <ApolloReactComponents.Query<TasksQuery, TasksQueryVariables> query={TasksDocument} {...props} />
+    );
+    
+export type TasksProps<TChildProps = {}> = ApolloReactHoc.DataProps<TasksQuery, TasksQueryVariables> & TChildProps;
+export function withTasks<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
   TasksQuery,
-  TasksQueryVariables
->
+  TasksQueryVariables,
+  TasksProps<TChildProps>>) {
+    return ApolloReactHoc.withQuery<TProps, TasksQuery, TasksQueryVariables, TasksProps<TChildProps>>(TasksDocument, {
+      alias: 'tasks',
+      ...operationOptions
+    });
+};
+
+/**
+ * __useTasksQuery__
+ *
+ * To run a query within a React component, call `useTasksQuery` and pass it any options that fit your needs.
+ * When your component renders, `useTasksQuery` returns an object from Apollo Client that contains loading, error, and data properties 
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useTasksQuery({
+ *   variables: {
+ *      status: // value for 'status'
+ *   },
+ * });
+ */
+export function useTasksQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<TasksQuery, TasksQueryVariables>) {
+        return ApolloReactHooks.useQuery<TasksQuery, TasksQueryVariables>(TasksDocument, baseOptions);
+      }
+export function useTasksLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<TasksQuery, TasksQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<TasksQuery, TasksQueryVariables>(TasksDocument, baseOptions);
+        }
+export type TasksQueryHookResult = ReturnType<typeof useTasksQuery>;
+export type TasksLazyQueryHookResult = ReturnType<typeof useTasksLazyQuery>;
+export type TasksQueryResult = ApolloReactCommon.QueryResult<TasksQuery, TasksQueryVariables>;
